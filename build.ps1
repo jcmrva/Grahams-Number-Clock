d91e9485fce0df62e41084104cd498591bd33497
Josh M <20140997+jcmrva@users.noreply.github.com>
2021-04-27 19:57:37 -0400

    move src folder up

--- build.ps1
+++ build.ps1
@@ -1,9 +1,3 @@
 
-Set-Location .\app -ErrorAction Stop
-
 elm-format .\src\ --yes
 elm make .\src\GrahamsNbrClock.elm --output=GrahamsNbrClock.js # --debug --optimize 
-
-Copy-Item -Path .\GrahamsNbrClock.js -Destination ..\GrahamsNbrClock.js -Force
-
-Set-Location ..
